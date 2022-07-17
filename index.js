ffe37f538958a6a3f5c2b1458eb3838fdeff69bd
BarariuBeniamin <barariubeniamin@gmail.com>
2022-07-17 19:15:36 +0300

    retusat css

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
 console.info(2);
 console.debug(33 + 2);
-console.warn(23* 3 - 10)
+console.warn(23 * 3 - 10);
 console.error("started");
 
 var myName = "Beniamin";
 var age = 29;
-console.info("My name is " +  myName + ", I'm " + age + "years old");
-age = 30
-console.info("My name is " +  myName + ", I'm " + age + "years old");
-age = 36
-console.info("My name is " +  myName + ", I'm " + age + "years old");
+console.info("My name is " + myName + ", I'm " + age + "years old");
+age = 30;
+console.info("My name is " + myName + ", I'm " + age + "years old");
+age = 36;
+console.info("My name is " + myName + ", I'm " + age + "years old");
 
 var jobtitle = " <span>Student</span> developer@gmail.com ";
-var mottoElement = document.getElementById("motto")
+var mottoElement = document.getElementById("motto");
 
 console.info(mottoElement);
 console.warn(mottoElement.innerHTML);
 
 // mottoElement.innerHTML = mottoElement.innerHTML + "&" jobtitle
-mottoElement.innerHTML = jobtitle
+mottoElement.innerHTML = jobtitle;
+
+// Show home page
+function showHomePage() {
+  document.getElementById("home").style.display = "block";
+}
+
+function showSkillsPage() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "block";
+}
+
+function showProjectsPage() {
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "block";
+}
+
+function showLanguagesPage() {
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "block";
+}
+
+showHomePage();
